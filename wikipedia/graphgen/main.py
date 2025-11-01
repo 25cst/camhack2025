032a4ae1c0a153a633262d78bea320889584c8b6
Siriusmart <sirius@siri.ws>
2025-11-01 18:15:33 +0000

    lxml parser

--- wikipedia/graphgen/main.py
+++ wikipedia/graphgen/main.py
@@ -1,21 +1,33 @@
 from libzim.reader import Archive
 from pathlib import Path
-from html.parser import HTMLParser
+from lxml import etree
 
-import os
+parser = etree.HTMLParser()
 
 zim = Archive(Path("/home/siriusmart/.local/share/kiwix/wikipedia_en_top_nopic_2025-09.zim"))
 relations: dict[str, set[str]] = dict()
 
+
+c = 0
+
 for i in range(zim.all_entry_count):
     entry = zim._get_entry_by_id(i)
-    parser = HTMLParser()
-    parser.feed(bytes(entry.get_item().content).decode("UTF-8"))
-
-    break
+    root = etree.fromstring(bytes(entry.get_item().content).decode("UTF-8"), parser)
 
     outlinks = set()
 
-    relations[entry.title]
+    for a in root.xpath("//a"):
+        href: str = a.get('href')
+        if href == None:
+            continue
+        href = href.split("#")[0]
+        if zim.has_entry_by_title(href):
+            outlinks.add(href)
 
-    print()
+
+    relations[entry.title] = outlinks
+
+    c += 1
+
+    if c % 100 == 0:
+        print(c)
