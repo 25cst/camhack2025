af9c8cdddd1fc5fe3e6d0c598f2a393e6efa65eb
kjm86 <kjm86@cam.ac.uk>
2025-11-01 22:06:04 +0000

    Improve bfs using heuristic

--- wikipedia/suggest/readdb.py
+++ wikipedia/suggest/readdb.py
@@ -1,11 +1,12 @@
 id_to_title: dict[int, str] = {}
 title_to_id: dict[str, int] = {}
 
-relations: dict[int, set[int]] = dict()
+_relations_sets: dict[int, set[int]] = dict()
+relations: dict[int, list[int]] = dict()
 
 def connect_nodes(a, b):
-    relations[a].add(b)
-    relations[b].add(a)
+    _relations_sets[a].add(b)
+    _relations_sets[b].add(a)
 
 with open("../graphgen/cache.txt", "r") as f:
     lines = f.readlines()
@@ -13,7 +14,7 @@ with open("../graphgen/cache.txt", "r") as f:
         title = line.split("|")[0].strip()
         id_to_title[i] = title
         title_to_id[title] = i
-        relations[i] = set()
+        _relations_sets[i] = set()
 
     for line in lines:
         chunks = line.split('|')
@@ -21,3 +22,6 @@ with open("../graphgen/cache.txt", "r") as f:
         for chunk in chunks[1:]:
             neighbour_id = title_to_id[chunk.strip()]
             connect_nodes(title_id, neighbour_id)
+
+    for title_id in _relations_sets.keys():
+        relations[title_id] = sorted(_relations_sets[title_id], key=lambda x: len(_relations_sets[x]), reverse=True)
