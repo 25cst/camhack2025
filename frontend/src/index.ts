468b0ae161c702789c9a66d6404dab5d15729fc5
Siriusmart <sirius@siri.ws>
2025-11-02 17:19:20 +0000

    bump

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -172,7 +172,7 @@ app.get('/api/guess', (req, res) => {
 
     console.log(`Guess for target "${code}": "${guess}" in round ${roundNum}`);
     
-    fetch(`http://127.0.0.1:8081/gethint?guess=${guess}&secret=${codeMap.get(code)}&n=3&hint_level=10`)
+    fetch(`http://127.0.0.1:8081/gethint?guess=${guess}&secret=${codeMap.get(code)}&n=10&hint_level=10`)
         .then((response) => response.json())
         .then((json) => {
     const response: GuessResponse = json as GuessResponse
