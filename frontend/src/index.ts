39c4cd961295e97a2e5f665fe87bc46af75322bd
Siriusmart <sirius@siri.ws>
2025-11-02 13:56:48 +0000

    specs

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -7,3 +7,17 @@ app.listen(port, () => {
 })
 
 app.use(express.static('static'))
+
+/*
+GET /api/newgame
+Request: empty
+Response: { code: string }
+
+GET /api/guess?code=string&guess=string&round=int
+Request: { code: string, guess: string, round: int } (round is 1 to 5)
+Response: { code: string }
+
+/api/wordlist
+Request: empty
+Response: { words: string[] }
+*/
