1efd3cd4b7cbb6669560c5777ae2a10979f55d64
Siriusmart <sirius@siri.ws>
2025-11-02 10:00:59 +0000

    server specs

---
+++ wikipedia/suggest/server.py
@@ -0,0 +1,73 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from socketserver import ThreadingMixIn
+import json
+import os
+
+def wordlist_handler(body):
+    raise Exception("TODO")
+
+def gethint_handler(body):
+    raise Exception("TODO")
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        response = { 'type': 'error', 'reason': 'Not found' }
+        status = 404
+
+        if self.path == "/":
+            response = {'type': "hello", "reason": 'world'}
+            status = 200
+
+        self.send_response(status)  # Bad request
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # Get the size of the POST data
+        post_data = self.rfile.read(content_length)  # Read the POST data
+        # Assuming the POST data is JSON
+
+        response = { 'type': 'error', 'reason': 'Not found' }
+        status = 404
+        try:
+            data = json.loads(post_data)
+
+            match self.path:
+                case "/wordlist":
+                    response = wordlist_handler(data)
+                    status = 200
+                case "/gethint":
+                    response = gethint_handler(data)
+                    status = 200
+        except json.JSONDecodeError:
+            response = {'type': "error", "reason": 'Invalid JSON'}
+            status = 400
+        except Exception as e:
+            response = {'type': "error", "reason": str(e)}
+            status = 500
+
+        self.send_response(status)  # Bad request
+        self.send_header('Content-type', 'application/json')
+        self.end_headers()
+        self.wfile.write(json.dumps(response).encode('utf-8'))
+        
+class ThreadingSimpleServer(ThreadingMixIn, HTTPServer):
+    pass
+
+def getPort():
+    s = os.getenv("PORT")
+    if s == None:
+        return 8081
+    try:
+        return int(s)
+    except:
+        return 8081
+
+def run():
+    server = ThreadingSimpleServer(('127.0.0.1', getPort()), Handler)
+    print(f"Server started on 127.0.0.1:{getPort()}")
+    server.serve_forever()
+
+if __name__ == '__main__':
+    run()
